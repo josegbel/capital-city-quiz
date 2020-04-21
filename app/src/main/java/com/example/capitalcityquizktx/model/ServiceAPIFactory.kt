257c0ef13a5dcadad3e3113b18479f714a21e0ce
Jose Garcia <jose.gbel@gmail.com>
2020-04-21 18:03:43 +0100

    can sign up users in remote database

---
+++ app/src/main/java/com/example/capitalcityquizktx/model/ServiceAPIFactory.kt
@@ -0,0 +1,24 @@
+package com.example.capitalcityquizktx.model
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceAPIFactory {
+    private const val API_BASE_URL = "http://10.0.2.2:8080/"
+
+    fun createService(): IRegisterService {
+        val logging = HttpLoggingInterceptor()
+        logging.apply { logging.level = HttpLoggingInterceptor.Level.BODY }
+        val httpClient = OkHttpClient.Builder().addInterceptor(logging)
+
+        val builder = Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+
+        return builder
+            .client(httpClient.build())
+            .build().create(IRegisterService::class.java)
+    }
+}
--- app/src/main/java/com/example/capitalcityquizktx/ui/RegisterPresenter.kt
+++ app/src/main/java/com/example/capitalcityquizktx/ui/RegisterPresenter.kt
@@ -1,62 +1,36 @@
 package com.example.capitalcityquizktx.ui
 
 import android.util.Log
-import com.example.capitalcityquizktx.model.IRegisterService
+import com.example.capitalcityquizktx.model.ServiceAPIFactory
 import com.example.capitalcityquizktx.model.UserDetails
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import okhttp3.MediaType
-import okhttp3.RequestBody
-import org.json.JSONObject
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.coroutines.CoroutineContext
 
 
 class RegisterPresenter (val view : IRegisterView){
-    private val parentJob = Job()
 
-    private val coroutineContext: CoroutineContext
-        get() = parentJob + Dispatchers.Default
-
-    private val scope = CoroutineScope(coroutineContext)
-
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("http://10.0.2.2:8080/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    private val service: IRegisterService = retrofit.create(IRegisterService::class.java)
+    private val service = ServiceAPIFactory.createService()
 
     fun createNewUser(userDetails: UserDetails) {
-//        scope.launch {
+        GlobalScope.launch {
+            val call = service.createUser(userDetails)
 
-        val map = HashMap<String, Any>()
-        map["username"] = userDetails.username
-        map["password"] = userDetails.password
-        map["email"] = userDetails.email
-        map["name"] = userDetails.name
-        map["surname"] = userDetails.surname
+            call.enqueue(object : Callback<UserDetails?> {
+                override fun onResponse(
+                    call: Call<UserDetails?>,
+                    response: Response<UserDetails?>
+                ) {
+                    Log.d("RETRONET", "SUCCESS")
+                }
 
-        val params = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), JSONObject(map).toString())
-
-        val call = service.createUser(userDetails)
-
-        call.enqueue(object : Callback<UserDetails?> {
-
-            override fun onResponse(call: Call<UserDetails?>, response: Response<UserDetails?>) {
-                val user1: UserDetails? = response.body()
-                Log.d("RETRONET", "SUCCESS")
-            }
-
-            override fun onFailure(call: Call<UserDetails?>, t: Throwable) {
-                call.cancel()
-                Log.d("RETRONET", "FAILED")
-            }
-        })
+                override fun onFailure(call: Call<UserDetails?>, t: Throwable) {
+                    call.cancel()
+                    Log.d("RETRONET", "FAILED")
+                }
+            })
         }
     }
+}
